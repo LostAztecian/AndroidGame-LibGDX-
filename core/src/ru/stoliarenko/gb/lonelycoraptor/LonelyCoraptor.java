bcc5cfbedd3c26ee47d8af0f36ab92edd0ee295e
Alexander <lostaztecian@mail.ru>
2018-11-22 17:55:50 +0300

    wp change

--- core/src/ru/stoliarenko/gb/lonelycoraptor/LonelyCoraptor.java
+++ core/src/ru/stoliarenko/gb/lonelycoraptor/LonelyCoraptor.java
@@ -5,23 +5,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class LonelyCoraptor extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	TextureRegion region;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("wp_space.jpg");
+		region = new TextureRegion(img, 0, 0, 1024, 1024);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(region, 0, 0);
 		batch.end();
 	}
 	
